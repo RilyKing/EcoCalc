7ddd0e1207a9e55cbf46d12735b70b727bab8ec2
RilyKing <rileyschoolnkc@gmail.com>
2024-10-02 18:06:37 +0000

    update index.py

--- index.py
+++ index.py
@@ -1,2 +1,10 @@
-riley = 'The best person ever'
-print(riley)
+#imports
+import tkinter as tk
+#start of tk window
+mw = tk.Tk()
+mw.title('EcoCalc')
+#add widgets here nd below
+mw.title('EcoCalc')
+button = tk.Button(mw, text='Idk', width=25, command=mw.destroy)
+#end of tk window
+mw.mainloop()
